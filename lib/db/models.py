f5b3b560235c6e447ac2f7c829d090b58df35724
KanaeBunche <125619060+KanaeBunche@users.noreply.github.com>
2023-08-08 09:18:29 -0400

    Changes aded the two table

--- lib/db/models.py
+++ lib/db/models.py
@@ -9,21 +9,25 @@ class Region(Base):
     
     id = Column(Integer, primary_key=True)
     region = Column(String, unique=True)
-    recipes = Column(String, unique = False)
-
+    recipes = Column(String, unique=False)
 
 class Recipe(Base):
     __tablename__ = "recipes"
-    
-    id = Column(Integer, primary_key=True)
-    title = Column(String, unique=True)
-    instructions = Column(String)
-    ingredients = Column(String)
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    ingredients= Column(String)
     region_id = Column(Integer, ForeignKey('regions.id'))
-    
-    
-    
+
+class UserList(Base):
+    __tablename__ = "userlist"
+
+    id= Column(Integer, primary_key=True)
+    ingredients = Column(String)
+    region_id = Column(Integer, ForeignKey('region.id'))
+
+
+
 db_url = 'sqlite:///sql_food.db'
 engine = create_engine(db_url)
 Session = sessionmaker(bind=engine)
@@ -31,15 +35,35 @@ session = Session()
 
 Base.metadata.create_all(engine)
 
-new_region = Region(region='Italy', recipes='Spaghetti')
+duplicate_regions = session.query(Region).filter_by(region='Italy').all()
+for region in duplicate_regions:
+    session.delete(region)
+session.commit()
+
+
+new_region = Region(region = 'Italy', recipes='Spaghetti')
 session.add(new_region)
 session.commit()
 
-new_recipe = Recipe(title='Spaghetti', instructions='Cook pasta', ingredients='Spaghetti', region_id=new_region.id)
+#creating a new recepie associated with the region
+new_recipe = Recipe(name='Sandwhich', ingredients='meat', region_id=new_region.id)
 session.add(new_recipe)
+session.commit()
+
+new_userlist = UserList(region = 'South Africa', ingredients='Tomatoes, pasta',region_id=new_region.id)
+session.add(new_region)
 session.commit()
 
 
 region_query = session.query(Region).filter_by(region='Italy').first()
-print(region_query)
+print("Region:",region_query.region)
+print("Recipe:",region_query.recipes)
 
+recipe_query = session.query(Recipe).filter_by(title='Sandwhich').first()
+print("Recipe:", recipe_query.title)
+print("Ingredients:", recipe_query.ingredients)
+print("Region:", recipe_query.region.region)
+
+userlist_query = session.query(UserList).filter_by(ingredients='Tomatoes, pasta').first()
+print("User List Ingredients:", userlist_query.ingredients)
+print("User List Region:", userlist_query.region.region)
