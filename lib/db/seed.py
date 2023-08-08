0ad8e742f4d9bb21429cc1cef28eef267d460667
KanaeBunche <125619060+KanaeBunche@users.noreply.github.com>
2023-08-08 13:12:32 -0400

    seed-py

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,37 +1,35 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Region, Recipe, UserList
+from models import Base, Region, Recipe, Ingredients, UserList
 
 def seed_database():
-    # Initialize database connection
-    engine = create_engine('sqlite:///sql_food.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    
 
-    # Create sample data
     regions_data = [
-        {"name": "Italy", "recipes": "Spaghetti, Meatballs"},
-        # {"name": "Paris", "recipes": "Fish, Bread"},
-        # {"name": "United States of America", "recipes": "GroundBeef, Bread"}
-        # Add more regions
+        {"name": "Italy", "recipes": "Spaghetti, Meatballs"}
+        
     ]
 
     for region_data in regions_data:
-        region = Region(region_data)
+        region = Region(**region_data)
         session.add(region)
         session.commit()
 
-    #     recipes_data = [
-    #     {"name": "Italy", "description": "You Put the noodels in the water","ingredients" :"Noodles, GroundBeef,Tomatoe Sauce"},
-    #     {"name": "Paris", "description": "You Put the Fish in the fryer","ingredients" :"GroundBeef, Bread"},
-    #     {"name": "United States of America", "description": "You Put the ground beef in the hot pan", "ingredients": "GroundBeef, Bread"}
-    #     # Add more recipes
-    # ]
+        recipes_data = [
+        {"name" : "Italy", "description": "You Put the noodels in the water","ingredients" :"Noodles, GroundBeef,Tomatoe Sauce"},
+        {"name": "Paris", "description": "You Put the Fish in the fryer","ingredients" :"GroundBeef, Bread"},
+        {"name": "United States of America", "description": "You Put the ground eef in the hot pan", "ingredients": "GroundBeef, Bread"}
+        # Add more recipes
+    ]
 
-    # for recipe_data in recipes_data:
-    #     recipe = Recipe(recipe_data)
-    #     session.add(recipe)
-    #     session.commit()
+    for recipe_data in recipes_data:
+        recipe = Recipe(**recipe_data)
+        session.add(recipe)
+        session.commit()
+    
+    engine = create_engine('sqlite:///sql_food.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
     # Add more sample data for recipes and user lists
 
